85135930ae8570ce66297dc4388dbcac2d4ed9a5
Damian Jeleń <damian.jelen.1986@gmail.com>
2022-10-25 21:27:44 +0200

    add default web filter

---
+++ src/main/java/pl/damjel/usercrud/filters/DefaultFilter.java
@@ -0,0 +1,22 @@
+package pl.damjel.usercrud.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "DefaultFilter")
+public class DefaultFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        chain.doFilter(request, response);
+    }
+}
